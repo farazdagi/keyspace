b5cc9f1dd72bfca9044b800ffd64d52f08093599
Victor Farazdagi <simple.square@gmail.com>
2025-05-05 01:17:40 +0300

    feat: define main traits

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# rs-keyspace
-Keyspace partitioning using random slicing heuristics.
+# keyspace
+
+[![crates.io](https://img.shields.io/crates/d/keyspace.svg)](https://crates.io/crates/keyspace)
+[![docs.rs](https://docs.rs/keyspace/badge.svg)](https://docs.rs/keyspace)
+
+Key space partitioning and re-balancing for distributed systems.
+
+## Motivation
+
+Implement a key space partitioning and re-balancing algorithm that is:
+
+- [ ] Memory/space efficient: scalable, no virtual nodes, for instance.
+- [ ] Fair: data is evenly distributed across partitions.
+- [ ] Compact: to compute the target node of a key, we only need to know the number of nodes `n`.
+- [ ] Adaptive: supports node addition and removal, with close to theoretically minimal data
+  movement.
+- [ ] Robust: supports replication out of the box.
+
+The idea is to allow system to grow to thousands of nodes, and to process millions of keys
+per second efficiently.
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::hash::Hash;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Node that serves as a destination for data.
+///
+/// Node controls one or more interval of the key space.
+/// Keys which fall into such an interval are routed to the node.
+pub trait Node: Hash {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Key space manager.
+///
+/// Allows to partition the key space, obtain the node responsible for a key,
+/// remove a node and re-balance the key space, so that data is evenly
+/// distributed across the nodes.
+///
+/// The implementation is assumed to be thread-safe.
+pub trait Keyspace<N: Node> {
+    /// A half-open interval of the key space.
+    ///
+    /// Range bounded inclusively below and exclusively above i.e.
+    /// `[start..end)`.
+    type Interval;
+
+    /// Add a node to the key space.
+    ///
+    /// Depending on the implementation, the node will claim one or more
+    /// intervals of the key space.
+    fn add(&self, node: N);
+
+    /// Remove a node from the key space.
+    fn remove(&self, node: &N);
+
+    /// Returns the node responsible for the given key.
+    ///
+    /// Due to replication, a key may land on several nodes, this method returns
+    /// the primary node responsible for the key.
+    /// Whenever more than one node is needed, use
+    /// [`replicas()`](Self::replicas).
+    ///
+    /// If the key space is empty (no nodes has been added), `None` is returned.
+    fn node<K: Hash>(&self, key: &K) -> Option<&N>;
+
+    /// Returns `k` nodes responsible for the given key.
+    ///
+    /// The first node is the primary node responsible for the key. It is
+    /// guaranteed that the first node is the same as the one returned by
+    /// [`node()`](Self::node).
+    fn replicas<K: Hash>(&self, key: &K, k: usize) -> Option<Vec<&N>>;
+
+    /// Returns intervals of the key space controlled by the given node.
+    ///
+    /// This method is necessary to re-balance the key space. When a node is
+    /// added or removed, data needs to be moved from one node to another.
+    /// In order to do so, the current intervals controlled by the node need
+    /// to be known.
+    ///
+    /// Whenever the node is not part of the key space, `None` is returned.
+    fn intervals(&self, node: &N) -> Option<Vec<Self::Interval>>;
 }
